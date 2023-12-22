6283db0fe8cf175ec92f3721452bc3995b71226e
Gabriel Moura Passos de Souza <gabriel.mpsouza@aluno.educacao.pe.gov.br>
2023-12-21 22:52:02 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@
 mv ~/Offspring/source/* /
 
 # Move o script ~/Offspring/offspring.sh para /usr/local/bin
-mv ~/Offspring/offspring.sh /usr/local/bin
+mv ~/Offspring/offspring /usr/local/bin
+mv ~/Offspring/offspring-english /usr/local/bin
 
 # Dá permissão de execução para o script em /usr/local/bin
-chmod +x /usr/local/bin/offspring.sh
+chmod +x /usr/local/bin/offspring
+chmod +x /usr/local/bin/offspring-english
